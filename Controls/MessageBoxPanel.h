b8cd88f311d79d5fee89d7e9cd4600b3973ec2dd
MichalFrankel <48519009+MichalFrankel@users.noreply.github.com>
2020-07-31 17:01:14 +0300

    Update and rename MessageBox.h to MessageBoxPanel.h

--- Controls/MessageBoxPanel.h
+++ Controls/MessageBoxPanel.h
@@ -3,28 +3,29 @@
 
 using namespace std;
 
-class MessageBox:public Panel{
+class MessageBoxPanel :public Panel {
+
 public:
-	MessageBox(int height, int width);
-	virtual ~MessageBox() = default;
+	MessageBoxPanel(int height, int width);
+	virtual ~MessageBoxPanel() = default;
 	virtual string getText();
-    virtual void setLayer(size_t layer);
-    virtual string getResult();
-    virtual void show();
+	virtual void setLayer(size_t layer);
+	virtual string getResult();
+	virtual void show();
 	virtual void setText(string text);
 	string getTitle();
 	virtual void setTitle(string title);
 
 protected:
 	virtual void addControl(Control& control, int left, int top);
-	struct AnswerListener : public MouseListener{
-		AnswerListener(MessageBox &box):_box(box){}
-		void mousePressed(Button &b, int x, int y, bool isLeft){
+	struct AnswerListener : public MouseListener {
+		AnswerListener(MessageBoxPanel& box) :_box(box) {}
+		void mousePressed(Button& b, int x, int y, bool isLeft) {
 			_box.hide();
 			_box._result = b.getText();
 		}
 	private:
-		MessageBox &_box;
+		MessageBoxPanel& _box;
 		size_t _index;
 	};
 protected:
